0b31567b0ef4af349fefb2c0e13453a3c29c5956
Daniel Wust <danielwust99@gmail.com>
2025-04-29 21:47:52 -0300

    removed eventEmitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,35 +5,23 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MeuFormComponent, CommonModule],
+  imports: [
+    MeuFormComponent,
+    CommonModule,
+  ],
+  styles: [` h1 { color: blue; } `],
   template: `
     <h1>Componente Pai</h1>
+    <p>Nome digitado no filho: {{ nomeDigitadoFilho() }}</p>
+    <p>Contador do filho: {{ contadorFilho() }}</p>
+
     <app-meu-form
       [contador]="contadorFilho"
       [nomeDigitado]="nomeDigitadoFilho"
-      (atualizarNome)="atualizarNomeDigitado($event)"
-      (incrementarContador)="incrementar()"
     ></app-meu-form>
-    <p>Nome digitado no filho: {{ nomeDigitadoFilho() }}</p>
-    <p>Contador do filho: {{ contadorFilho() }}</p>
   `,
-  styles: [`
-    h1 {
-      color: blue;
-    }
-  `],
 })
 export class AppComponent {
   nomeDigitadoFilho = signal('');
   contadorFilho = signal(0);
-
-  incrementar() {
-    this.contadorFilho.update(c => c + 1)
-  }
-
-  atualizarNomeDigitado(novoNome: any | string) {
-    this.nomeDigitadoFilho.update((old: string) =>
-      old != novoNome ? novoNome : old
-    );
-  }
 }
--- src/app/meu-form/meu-form.component.ts
+++ src/app/meu-form/meu-form.component.ts
@@ -1,51 +1,31 @@
-import { Component, Input, signal, WritableSignal, Output, EventEmitter } from '@angular/core';
+import { Component, Input, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Signal } from '@angular/core';
 
 @Component({
   selector: 'app-meu-form',
   standalone: true,
   imports: [FormsModule],
+  styleUrl: './meu-form.component.css',
   template: `
     <div>
-      <h2>Digite seu nome:</h2>
-      <input type="text" [(ngModel)]="nome" (input)="atualizarNomeNoPai(nome)">
-      <button (click)="incrementar()">Clique aqui: {{ contadorInterno() }} vezes</button>
+      <h2>Componente Filho:</h2>
+      <h3>Digite seu nome:</h3>
+      <input type="text" [(ngModel)]="nome" (input)="atualizarNome(nome)">
+      <button (click)="incrementar()">Clique aqui: {{ contador() }} vezes</button>
     </div>
   `,
-  styles: [`
-    div {
-      border: 1px solid #ccc;
-      padding: 15px;
-      margin-bottom: 15px;
-    }
-    input[type="text"] {
-      margin-bottom: 10px;
-      padding: 8px;
-      width: 200px;
-    }
-    button {
-      padding: 10px 15px;
-      cursor: pointer;
-    }
-  `],
 })
 export class MeuFormComponent {
   nome = '';
 
-  @Input() nomeDigitado!: Signal<string>;
+  @Input() nomeDigitado!: WritableSignal<string>;
   @Input() contador!: WritableSignal<number>;
-  @Output() atualizarNome = new EventEmitter<string>();
-  @Output() incrementarContador = new EventEmitter<number>();
-
-  contadorInterno = signal(0);
-
-  atualizarNomeNoPai(novoNome: string) {
-    this.atualizarNome.emit(novoNome);
-  }
 
   incrementar() {
-    this.contadorInterno.update(c => c + 1);
-    this.incrementarContador.emit(1);
+    this.contador.update(c => c + 1)
+  }
+
+  atualizarNome(novoNome: string) {
+    this.nomeDigitado.update(o => novoNome);
   }
 }
